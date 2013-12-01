398fd148b0fb3bc364eed92d165af2e177f251b5
Joel Berger <joel.a.berger@gmail.com>
2013-12-01 09:34:06 -0600

    renamed files to be more descriptive

--- ex/timers.pl
+++ ex/timers.pl
@@ -8,16 +8,16 @@ use Mojo::IOLoop;
 
 say Mojo::IOLoop->singleton->reactor;
 
-my $i = 0;
-my $id;
-$id = Mojo::IOLoop->recurring( 1 => sub { 
-  say 'tick ' . ++$i . '/3';
-  Mojo::IOLoop->remove($id) if $i == 3;
-});
-
-#Mojo::IOLoop->timer( 4 => sub {
-#  say 'Boom';
+#my $i = 0;
+#my $id;
+#$id = Mojo::IOLoop->recurring( 1 => sub { 
+#  say 'tick ' . ++$i . '/3';
+#  Mojo::IOLoop->remove($id) if $i == 3;
 #});
+
+Mojo::IOLoop->timer( 4 => sub {
+  say 'Boom';
+});
 
 Mojo::IOLoop->start;
 
